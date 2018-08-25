06ff23907d71a5aac5bdd73a12b63cca5399b6c2
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2018-08-25 13:57:50 +0800

    Add files via upload

---
+++ invattack.py
@@ -0,0 +1,55 @@
+from Linephu.linepy import *
+from Linephu.akad.ttypes import *
+
+client = LINE()
+
+oepoll = OEPoll(client)
+
+whitelist = ["u52afe1d4ea5332242efacfeb9190d2a3"]
+
+def lineBot(op):
+    try:
+        if op.type == 26:
+            msg = op.message
+            try:
+                if msg.toType == 2:
+                    if msg._from in whitelist:
+                        if msg.text.startswith("/invattack"):
+                            str1 = find_between_r(msg.text, "/invattack ", "")
+                            targets = []
+                            targets.append(str1)
+                            client.findAndAddContactsByMid(str1)
+                            groupname = "炸三小 有叫你炸是不是"
+                            for target in targets:
+                                client.createGroup(groupname, [target])
+                                print("啟動")
+                            client.sendMessage(msg.to, "啟動")
+                    else:
+                        pass
+                else:
+                    pass
+            except:
+                pass
+        else:
+            pass
+    except Exception as e:
+        print(e)
+        return
+
+def find_between_r(s, first, last):
+    try:
+        start = s.rindex(first) + len(first)
+        end = s.rindex(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
+while True:
+    try:
+        ops = oepoll.singleTrace(count=50)
+        if ops is not None:
+            for op in ops:
+                lineBot(op)
+                oepoll.setRevision(op.revision)
+    except Exception as e:
+        print(e)
