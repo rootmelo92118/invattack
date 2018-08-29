f15a199b8bcf8310a37cf05cea2f46d34e8990f5
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2018-08-29 15:18:47 +0800

    Add files via upload

---
+++ invattack.py
@@ -0,0 +1,58 @@
+from Linephu.linepy import *
+from Linephu.akad.ttypes import *
+
+client = LINE()
+
+oepoll = OEPoll(client)
+
+whitelist = ["u52afe1d4ea5332242efacfeb9190d2a3"]
+
+def NOTIFIED_INVITE_INTO_GROUP(op):
+    try:
+        client.acceptGroupInvitation(op.param1)
+    except Exception as e:
+        print(e)
+        print("\n\nNOTIFIED_INVITE_INTO_GROUP\n\n")
+        return
+
+def RECEIVE_MESSAGE(op):
+    msg = op.message
+    try:
+        if msg.toType == 2:
+            if msg._from in whitelist:
+                if msg.text.startswith("/invattack"):
+                    str1 = find_between_r(msg.text, "/invattack ", "")
+                    targets = []
+                    targets.append(str1)
+                    client.findAndAddContactsByMid(str1)
+                    groupname = "嫌人家群多? 好 我送你更多"
+                    for target in targets:
+                        client.createGroup(groupname, [target])
+                        print("啟動")
+                    client.sendMessage(msg.to, "啟動")
+            else:
+                pass
+        else:
+            pass
+    except Exception as error:
+        print(error)
+        print("\n\nRECEIVE_MESSAGE\n\n")
+        return
+
+
+oepoll.addOpInterruptWithDict({
+    OpType.RECEIVE_MESSAGE: RECEIVE_MESSAGE,
+    OpType.NOTIFIED_INVITE_INTO_GROUP: NOTIFIED_INVITE_INTO_GROUP
+})
+
+def find_between_r(s, first, last):
+    try:
+        start = s.rindex(first) + len(first)
+        end = s.rindex(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
+
+while True:
+    oepoll.trace()
